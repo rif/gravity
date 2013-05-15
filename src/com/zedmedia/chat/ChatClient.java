cf85fc515f4612b47830610ed9e0540280d86d81
Radu Ioan Fericean <radu@fericean.ro>
2013-05-15 16:35:45 +0300

    working version

--- src/com/zedmedia/chat/ChatClient.java
+++ src/com/zedmedia/chat/ChatClient.java
@@ -23,10 +23,10 @@ import android.widget.ListView;
 public class ChatClient extends Activity {
 	private ArrayList<String> messages = new ArrayList<String>();
 	private Handler handler = new Handler();
-	private XmppSettings settings;
 	private EditText recipient;
 	private EditText text;
 	private ListView list;
+	private XmppSettings settings;
 	private XMPPConnection connection;
 
 	/** Called when the activity is first created. */
@@ -40,14 +40,14 @@ public class ChatClient extends Activity {
 		list = (ListView) this.findViewById(R.id.messageList);
 		setListAdapter();
 
-		// Window for getting settings
 		settings = new XmppSettings(this);
 	}
 
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
-		String to = recipient.getText().toString();
-        String message = text.getText().toString();		
+		String to = recipient.getText().toString() + "@ded697.ded.reflected.net";
+		String message = text.getText().toString();		
+		text.setText("");
 
 		Message msg = new Message(to, Message.Type.chat);
 		msg.setBody(message);
@@ -85,7 +85,7 @@ public class ChatClient extends Activity {
 
 	private void setListAdapter() {
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.id.messageList, messages);
+				R.layout.list, messages);
 		list.setAdapter(adapter);
 	}
 
@@ -95,26 +95,20 @@ public class ChatClient extends Activity {
 		getMenuInflater().inflate(R.menu.chat_client, menu);
 		return true;
 	}
-
-	/*
-	 * @Override public boolean onCreateOptionsMenu(Menu menu) { MenuInflater
-	 * inflater = getMenuInflater(); inflater.inflate(R.menu.menu, menu); return
-	 * true; }
-	 */
-
+	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.settings:
-			settings.show();
-			return true;
-		case R.id.quit:
-			finish();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-
-	}
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+        case R.id.settings:
+            settings.show();
+            return true;
+        case R.id.quit:
+            finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    
+    }
 
 }
